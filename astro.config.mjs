7cca8d6ae9f2ce495545f5208025861ed5eac4be
EZWD <54875049+EZWD@users.noreply.github.com>
2025-08-30 00:08:23 -0400

    move to customized domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,8 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://ezwd.github.io/p-web-page/',
-  base: '/p-web-page/',
+  // site: 'https://ezwd.github.io/p-web-page/',
+  // base: '/p-web-page/',
+  site: 'https://peiyuzhang.com',
   integrations: [mdx(), sitemap(), tailwind()]
 });
