3da5618dbe33e0b15eba5f40609f8ae6be5276fb
EZWD <54875049+EZWD@users.noreply.github.com>
2025-11-26 20:36:37 -0500

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Peiyu Zhang - University of Virginia (UVA) | Portfolio';
+export const SITE_TITLE = 'Peiyu Zhang';
 export const SITE_DESCRIPTION = 'The personal website of Peiyu Zhang, a Systems and Information Engineering PhD student/researcher at the University of Virginia (UVA) specializing in Human-Computer Interaction/Human Factors. View projects, resume, and contact info.';
 export const GENERATE_SLUG_FROM_TITLE = true
 export const TRANSITION_API = true
